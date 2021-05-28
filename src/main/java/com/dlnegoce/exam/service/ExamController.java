9c58f414539e5ce6acfa7f6ba4b582ec5dea9bef
dg172856 <gaetan.diligence@gmail.com>
2021-05-28 14:46:29 +0200

    Feat: webservice GET('catalog-stats') implement + display result on p

--- src/main/java/com/dlnegoce/exam/service/ExamController.java
+++ src/main/java/com/dlnegoce/exam/service/ExamController.java
@@ -1,6 +1,7 @@
 package com.dlnegoce.exam.service;
 
 import com.dlnegoce.exam.pojo.CatalogPOJO;
+import com.dlnegoce.exam.pojo.ItemPOJO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ExamController {
@@ -16,12 +20,7 @@ public class ExamController {
     public Object catalog() {
         // TODO : load catalog.json file to the appropriate java object and return it
         try {
-            File resource = new ClassPathResource(
-                    "catalog.json").getFile();
-            String json = new String(
-                    Files.readAllBytes(resource.toPath()));
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(json, CatalogPOJO.class);
+            return readCatalog("catalog.json");
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -34,7 +33,43 @@ public class ExamController {
         // - total number of nodes
         // - count level 1 and 2 based on level 3 and show results
         // TODO : And return statistics results with the appropriate object
+        try {
+            CatalogPOJO catalog = readCatalog("catalog.json");
+            List<String> codeLevel1 = new ArrayList<>();
+            List<String> codeLevel2 = new ArrayList<>();
+            List<String> codeLevel3 = new ArrayList<>();
+
+            for(ItemPOJO childLevel1 : catalog.getChildren())
+                for (ItemPOJO childLevel2 : childLevel1.getChildren())
+                    for (ItemPOJO childLevel3 : childLevel2.getChildren()){
+                        String[] codes = childLevel3.getPath().split("/");
+                        if(!codeLevel1.contains(codes[1]))
+                            codeLevel1.add(codes[1]);
+                        if(!codeLevel2.contains(codes[2]))
+                            codeLevel2.add(codes[2]);
+                        if(!codeLevel3.contains(codes[3]))
+                            codeLevel3.add(codes[3]);
+                    }
+
+            return "{" +
+                    "\"numberOfNodes\":" + (codeLevel1.size() + codeLevel2.size() + codeLevel3.size()) +
+                    ",\"numberOfLevel1\":" + codeLevel1.size() +
+                    ",\"numberOfLevel2\":" + codeLevel2.size() +
+                    ",\"numberOfLevel3\":" + codeLevel3.size() +
+                    "}";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private CatalogPOJO readCatalog(String path) throws Exception{
+        File resource = new ClassPathResource(
+                path).getFile();
+        String json = new String(
+                Files.readAllBytes(resource.toPath()));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, CatalogPOJO.class);
     }
 }
 
--- src/main/resources/static/catalog.js
+++ src/main/resources/static/catalog.js
@@ -16,4 +16,27 @@ function requestCatalog() {
     httpRequest.send();
 }
 
+function requestCatalogStat() {
+    let text = document.getElementById("catalogStats");
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest)
+        return false;
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let json = JSON.parse(httpRequest.responseText);
+                text.innerHTML = "Number of nodes: " + json["numberOfNodes"] + "</br>" +
+                "Number of nodes level 1: " + json["numberOfLevel1"] + "</br>" +
+                "Number of nodes level 2: " + json["numberOfLevel2"] + "</br>" +
+                "Number of nodes level 3: " + json["numberOfLevel3"];
+            } else {
+                text.innerHTML = "Erreur " + httpRequest.status;
+            }
+        }
+    };
+    httpRequest.open("GET", "/catalog-stats");
+    httpRequest.send();
+}
+
 requestCatalog();
+requestCatalogStat();
